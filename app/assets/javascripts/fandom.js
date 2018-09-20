ba1dc67b3566f385cac7811fb3bc2087e29171eb
VictoriaMeng <1ststarofthenight@gmail.com>
2018-09-19 20:02:39 -0700

    Add get request to franchise show link that successfully retrieves franchise instance in json format

--- app/assets/javascripts/fandom.js
+++ app/assets/javascripts/fandom.js
@@ -14,8 +14,9 @@ function addIndexListeners() {
   $("ol li a").each(function() {
     this.addEventListener("click", function(event) {
       event.preventDefault();
-      alert(this);
-      // debugger;
+      $.get(this, function(franchise) {
+        debugger;
+      });
     })
   });
 };
