0526e219ff2bae79cd795fd63b2c9ad911294363
VictoriaMeng <1ststarofthenight@gmail.com>
2018-09-20 21:20:16 -0700

    Add average_rating and ratings_count attributes to Franchise Serializer

--- app/serializers/franchise_serializer.rb
+++ app/serializers/franchise_serializer.rb
@@ -1,4 +1,12 @@
 class FranchiseSerializer < ActiveModel::Serializer
-  attributes :id, :name, :medium
+  attributes :id, :name, :medium, :average_rating, :ratings_count
   has_many :ratings
+
+  def average_rating
+    "#{object.average_rating}"
+  end
+
+  def ratings_count
+    "#{object.count_ratings}"
+  end
 end
