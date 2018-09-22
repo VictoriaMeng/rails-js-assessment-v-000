cf1cfd2fdfc5d1b34479fc73c450fe96c96a0ff6
VictoriaMeng <1ststarofthenight@gmail.com>
2018-09-22 12:14:50 -0700

    Write and invoke function to compileShowIndex when #franchise-show div is ready

--- app/assets/javascripts/franchises/show.js
+++ app/assets/javascripts/franchises/show.js
@@ -1,6 +1,10 @@
 $("#franchise-show").ready(function() {
-
+  compileShowIndex();
 });
+
+function compileShowIndex() {
+  showTemplate = Handlebars.compile($("#franchise-show")[0].innerHTML);
+};
 
 
 
