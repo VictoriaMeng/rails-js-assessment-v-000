dae4d6b9287d41152af95f5646e31d99407dd213
VictoriaMeng <1ststarofthenight@gmail.com>
2018-09-23 11:43:40 -0700

    Initialize previousId and nextId variables based on <p> value of current franchise ID. Allow renderFranchise to accept href parameter instead of making window.location.href the default. Pass previousId and nextId as arguments into renderFranchise() to append franchise data to page w/o refresh

--- app/assets/javascripts/franchises/show.js
+++ app/assets/javascripts/franchises/show.js
@@ -1,24 +1,32 @@
 $("#franchise-show").ready(function() {
   compileShowTemplate();
-  renderFranchise();
+  renderFranchise(window.location.href);
 });
 
 function compileShowTemplate() {
   showTemplate = Handlebars.compile($("#franchise-show-template")[0].innerHTML);
 };
 
-function renderFranchise() {
-  $.getJSON(window.location.href, function(franchise) {
+function renderFranchise(href) {
+  $.getJSON(href, function(franchise) {
     $("#franchise-show").prepend(showTemplate(franchise));
   });
 };
 
 function showPrevious() {
-  alert("Previous!");
+  const previousId = parseInt($("p").attr("value")) - 1;
+  clearFranchise();
+  renderFranchise("/franchises/" + previousId);
 };
 
 function showNext() {
-  alert("Next!")
+  const nextId = parseInt($("p").attr("value")) + 1;
+  clearFranchise();
+  renderFranchise("/franchises/" + nextId);
 };
+
+function clearFranchise() {
+  $("#franchise-show")[0].innerHTML = "";
+}
 
 
