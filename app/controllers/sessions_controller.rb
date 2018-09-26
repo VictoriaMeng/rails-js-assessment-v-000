9dd8199d64764afead8af632f3665a4cc5a2ec2a
VictoriaMeng <1ststarofthenight@gmail.com>
2018-09-25 20:45:14 -0700

    Add conditional to sessions controller create action where login fails without a correct password confirmation. Adds flash error message when login fails

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,10 +7,11 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(name: params[:user][:name])
-    if @user && @user.authenticate(params[:user][:password])
+    if @user && @user.authenticate(params[:user][:password]) && @user.authenticate(params[:user][:password_confirmation])
       session[:user_id] = @user.id.to_s
       redirect_to user_path(@user)
     else
+      flash[:errors] = "Incorrect login. Try again."
       redirect_to login_path
     end
   end
