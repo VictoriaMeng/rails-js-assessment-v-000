43e694281640adda922727ecf2927039611a0fb7
VictoriaMeng <1ststarofthenight@gmail.com>
2018-09-28 14:42:34 -0700

    Begin to change Franchise class constructor to fully convert Franchise json object into js model object. Add id, name, medium, and url attributes that are defined in constructor

--- app/assets/javascripts/franchises/franchise.js
+++ app/assets/javascripts/franchises/franchise.js
@@ -1,5 +1,9 @@
 class Franchise {
   constructor(franchise) {
+    this.id = id;
+    this.name = name;
+    this.medium = medium;
+    this.url = url;
     this.ratings_count = franchise["ratings_count"];
     this.average_rating = franchise["average_rating"];
   };
@@ -12,3 +16,30 @@ class Franchise {
     return `Average Rating: ${this.average_rating}`
   };
 }
+
+// {
+
+//   "id": 1,
+//   "name": "Steven Universe",
+//   "medium": "TV Show",
+//   "average_rating": 5.0,
+//   "ratings_count": 2,
+//   "url": "/franchises/1",
+//   "your_rating": {
+//       "id": 4,
+//       "stars": 5,
+//       "user_id": 2293352840680163,
+//       "franchise_id": 1
+//   },
+//   "ratings": [
+//       {
+//           "id": 1,
+//           "stars": 5
+//       },
+//       {
+//           "id": 4,
+//           "stars": 5
+//       }
+//   ]
+
+// }
